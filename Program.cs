4f36847d6d468b8d820e7164e112acef98934646
Joey <smithjoey5123@gmail.com>
2018-08-27 10:29:37 -0500

    classes practice

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace classes
+{
+    class Program
+    {
+        public class Employee
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Title { get; set; }
+            public DateTime StartDate { get; set; }
+        }
+
+        public class Company
+        {
+            public DateTime DateFounded { get; set; }
+            public string CompanyName { get; set; }
+            public List<Employee> Employees = new List<Employee>();
+
+            public void ListEmployees() {
+
+                foreach (Employee employee in Employees)
+                {
+                    Console.WriteLine($"{employee.FirstName} {employee.LastName}: {employee.Title}");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Company SomeCompany = new Company() {
+                DateFounded = DateTime.Now,
+                CompanyName = "That Company"
+            };
+
+            Employee Howard = new Employee() {
+                FirstName = "Howard",
+                LastName = "Trice",
+                Title = "A person",
+                StartDate = DateTime.Now,
+            };
+
+            Employee Jack = new Employee() {
+                FirstName = "Jack",
+                LastName = "Snow",
+                Title = "Programmer",
+                StartDate = DateTime.Now,
+            };
+
+            Employee Fred = new Employee() {
+                FirstName = "Fred",
+                LastName = "Hill",
+                Title = "Maid",
+                StartDate = DateTime.Now,
+            };
+
+            SomeCompany.Employees.Add(Howard);
+            SomeCompany.Employees.Add(Jack);
+            SomeCompany.Employees.Add(Fred);
+
+            SomeCompany.ListEmployees();
+        }
+    }
+}
